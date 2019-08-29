80ec78ded8496f2ae589a4c7aa06f1cb67f49988
Hamade <mmdarrah@hotmail.com>
2019-08-29 10:25:10 +0200

    fixing form bug

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from 'react'
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1 className="contact">I'd love to talk! Email me at the address below</h1>
-    <p className="contactp">
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
     <form
       name="contact-form"
       method="post"
@@ -16,6 +17,7 @@ export default () => (
       <textarea name="message" />
       <button>Send</button>
     </form>
-    </p>
-  </Layout>
+  </div>
 )
+
+export default IndexPage
