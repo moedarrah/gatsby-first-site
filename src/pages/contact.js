3804673a7b27138c989f2a6538ce4652b8443fa3
Hamade <mmdarrah@hotmail.com>
2019-08-29 11:14:41 +0200

    back to

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,53 +1,11 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/layout"
 
-const Contact = () => (
+export default () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+    <h1 className="contact">I'd love to talk! Email me at the address below</h1>
+    <p className="contactp">
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
   </Layout>
 )
-
-export default Contact
