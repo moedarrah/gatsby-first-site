8ab2f77498f2a665d7465136b07000569e75eb3b
Hamade <mmdarrah@hotmail.com>
2019-08-29 10:14:27 +0200

    fixing Hero-image size

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,8 +12,8 @@ export default () => (
       data-netlify-honeypot="bot-field"
     >
       <input name="name" placeholder="Your Name" type="text" />
-      {/* <input name="email" placeholder="name@name.com" type="email" />
-      <textarea name="message" /> */}
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
       <button>Send</button>
     </form>
     </p>
