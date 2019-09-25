e8bca7941d85fd5b3e94c472a5a0891bfa5f1b86
Hamade <mmdarrah@hotmail.com>
2019-09-25 14:03:45 +0200

    bug with netlify2

--- src/components/darkMode/DarkMode.js
+++ src/components/darkMode/DarkMode.js
@@ -8,30 +8,11 @@ import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
 const DarkMode = props => {
  
-  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  const [darkMode, setDarkMode] = React.useState(false);
   
-  function getInitialMode() {
-    const isReturningUser = "dark" in localStorage;
-    const savedMode = JSON.parse(localStorage.getItem("dark"));
-    const userPrefersDark = getPrefColorScheme();
-    // if mode was saved --> dark / light
-    if (isReturningUser) {
-      return savedMode;
-      // if preferred color scheme is dark --> dark
-    } else if (userPrefersDark) {
-      return true;
-      // otherwise --> light
-    } else {
-      return false;
-    }
-    // return savedMode || false;
-  }
-  
-  function getPrefColorScheme() {
-    if (!window.matchMedia) return;
 
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
-  }
+  
+  
 
   return(
         
