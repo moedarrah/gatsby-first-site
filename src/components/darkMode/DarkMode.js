25e71148c759fa060a88322b5620635687f4bae4
Hamade <mmdarrah@hotmail.com>
2019-10-09 13:17:12 +0200

    trying local storeg

--- src/components/darkMode/DarkMode.js
+++ src/components/darkMode/DarkMode.js
@@ -8,7 +8,35 @@ import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
 const DarkMode = props => {
  
-  const [darkMode, setDarkMode] = React.useState(false);
+  
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  React.useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    const userPrefersDark = getPrefColorScheme();
+    // if mode was saved --> dark / light --------------------------------------------------------------------------target="_blank"
+    if (isReturningUser) {
+      return savedMode;
+      // if preferred color scheme is dark --> dark
+    } else if (userPrefersDark) {
+      return true;
+      // otherwise --> light
+    } else {
+      return false;
+    }
+    // return savedMode || false;
+  }
+  
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return;
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
   
   return(
         
